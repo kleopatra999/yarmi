20f27260bccafcb8898a842a6cee0e2b92c10e60
niXman <niXman@users.noreply.github.com>
2014-06-11 22:14:30 +0300

    Update yarmi.hpp

--- include/yarmi/yarmi.hpp
+++ include/yarmi/yarmi.hpp
@@ -49,9 +49,6 @@
 
 namespace yarmi {
 
-template<typename... R>
-void unpack(R...) {}
-
 template<typename Invoker, typename... Invokers>
 bool invoke(const char *ptr, const std::size_t size, id_type *cid, Invoker &head, Invokers&... tail) {
 	istream_type istream(ptr, size);
@@ -60,13 +57,14 @@ bool invoke(const char *ptr, const std::size_t size, id_type *cid, Invoker &head
 	iarchive & call_id;
 	if ( cid ) *cid = call_id;
 
-	bool result = false;
-	auto o = [&result](const id_type call_id, iarchive_type &iarchive, Invoker &invoker)->bool {
-		return result = result || invoker.invoke(call_id, iarchive);
+	bool flag = false;
+	auto apply  = [](...) {};
+	auto pred   = [](const id_type call_id, bool &flag, iarchive_type &iarchive, Invoker &invoker)->bool {
+		return flag=flag || invoker.invoke(call_id, iarchive);
 	};
-	unpack(o(call_id, iarchive, head), o(call_id, iarchive, tail)...);
+	apply(pred(call_id, flag, iarchive, head), pred(call_id, flag, iarchive, tail)...);
 
-	return result;
+	return flag;
 }
 
 } // ns yarmi
